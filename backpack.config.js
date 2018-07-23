42b561c325341ebfe40bf84889ace4619cd301f8
akshay upadhya <akshaysrinagar@gmail.com>
2018-07-23 23:45:18 +0530

    base setup

---
+++ backpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    webpack: (config, options, webpack) => {
+        config.entry.main = [
+            './src/main.ts'
+        ]
+
+        config.resolve = {
+            extensions: [".ts", ".js", ".json"]
+        };
+
+        config.module.rules.push({
+            test: /\.ts$/,
+            loader: 'awesome-typescript-loader'
+        });
+
+        return config
+    }
+}
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
+app.get('/', (req: Request, res: Response) => {
+  res.send('lets do it!!')
+})
+app.listen('3000', () => {
+  console.log(`listening on port 3000`)
+})

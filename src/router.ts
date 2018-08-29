4471904f45b764f22db2d929fcaf6033381e354c
akshay upadhya <akshaysrinagar@gmail.com>
2018-08-29 11:26:19 +0530

    linting router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,13 @@
-import { Router, Request, Response} from 'express'
-import auth from "./routes/auth";
+import { Router, Request, Response } from 'express'
+import auth from './routes/auth'
+import cns from './routes/cns'
+const router: Router = Router()
 
-const router :Router = Router()
+router.get('/', (req: Request, res: Response) => {
+  res.send('lets do it!!')
+})
 
-router.get("/", (req: Request, res: Response) => {
-  res.send("lets do it!!");
-});
-router.use("/auth", auth);
+router.use('/auth', auth)
+router.use('/cns', cns)
 
 export default router
